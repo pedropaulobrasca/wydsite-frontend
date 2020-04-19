39e3a72ec8e2a6f284e56d760a58201a6fb43295
Pedro Brasca <pedropaulobrasca@gmail.com>
2020-04-19 05:12:51 -0300

    Header na home

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import ParticlesBg from "particles-bg";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <ParticlesBg
+      color="#ffffff"
+      type="cobweb"
+      bg={true}
+      num={100}
+    />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,34 +1,40 @@
-import React, { useState } from "react";
+import React from "react";
 import { FaInstagram, FaFacebook, FaWhatsapp } from "react-icons/fa";
-import { useHistory } from "react-router-dom";
-import ParticlesBg from "particles-bg";
+import { ParallaxProvider } from "react-scroll-parallax";
 
 import "./styles.css";
 
 export default function Home(params) {
+  console.log(this);
   return (
-    <div className="home-container">
-      <header>
-        <span>WYD</span>
-        <ul>
-          <li>Home</li>
-          <li>Cadastro</li>
-          <li>Download</li>
-          <li>Guias</li>
-        </ul>
-        <ul>
-          <li>
-            <FaInstagram />
-          </li>
-          <li>
-            <FaFacebook />
-          </li>
-          <li>
-            <FaWhatsapp />
-          </li>
-        </ul>
-      </header>
-      <ParticlesBg color="#ffffff" type="cobweb" bg={true} num={100} />
-    </div>
+    <ParallaxProvider>
+      <div className="home-container">
+        <header className="header">
+          <ul className="menu">
+            <li>Home</li>
+            <li>Cadastro</li>
+            <li>Download</li>
+            <li>Guias</li>
+          </ul>
+          <ul className="social">
+            <li>
+              <a href="#">
+                <FaInstagram />
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <FaFacebook />
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <FaWhatsapp />
+              </a>
+            </li>
+          </ul>
+        </header>
+      </div>
+    </ParallaxProvider>
   );
 }
